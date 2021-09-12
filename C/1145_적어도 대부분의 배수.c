f48f331c15eba5076cabea196151413cbc6cc8d7
JunSpring <wjdwnsqja08@gmail.com>
2021-09-13 00:07:20 +0900

    BOJ

--- C/1145_적어도 대부분의 배수.c
+++ C/1145_적어도 대부분의 배수.c
@@ -3,27 +3,35 @@
 
 int main()
 {
-	int num[5];
+	int num[5], temp;
 	scanf("%d %d %d %d %d", &num[0], &num[1], &num[2], &num[3], &num[4]);
-	
-	int min = num[0];
-	for (int i = 1; i < 5; i++)
-		if (min > num[i]) min = num[i];
 
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (num[j] > num[j + 1])
+			{
+				temp = num[j];
+				num[j] = num[j + 1];
+				num[j + 1] = temp;
+			}
+		}
+	}
+
+	int min = num[0];
 	int answer, sum = 0;
-	for (answer = min; answer <= num[0] * num[1] * num[2] * num[3] * num[4]; answer++)
+	for (answer = min; answer <= num[2] * num[3] * num[4]; answer++)
 	{
 		for (int j = 0; j < 5; j++)
 		{
 			if (answer % num[j] == 0) sum++;
-			printf("%d\n", answer % num[j]);
 		}
-			
+
 
 		if (sum >= 3) break;
 		else sum = 0;
 	}
 
 	printf("%d", answer);
-	printf(" %d", sum);
 }
