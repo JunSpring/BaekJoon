f01f01d3f7a02017bf7ac4aea5303ad384d49585
JunSpring <wjdwnsqja08@gmail.com>
2022-02-09 00:14:12 +0900

    BOJ

--- C++/DR_NAM_IROC/02. 자료구조 1-스택  큐  덱  문자열 연습/1373_2진수 8진수.cpp
+++ C++/DR_NAM_IROC/02. 자료구조 1-스택  큐  덱  문자열 연습/1373_2진수 8진수.cpp
@@ -6,48 +6,31 @@ using namespace std;
 
 int main()
 {
-	int num, sum;
+	string num, temp;
+	int sum, rest;
 	stack<int> s;
 
 	cin >> num;
+	rest = num.size() % 3;
 
-	if (num == 0)
+	if (num.size()==1 && stoi(num) == 0)
 	{
 		cout << 0;
 		return 0;
 	}
 
-	while (num)
+	for (int i = 0; i < num.size() / 3; i++)
 	{
-		sum = 0;
-		
-		if (num % 1000 >= 100)
-		{
-			sum += num % 10;
-			num /= 10;
-			sum += 2 * num % 10;
-			num /= 10;
-			sum += 4 * num % 10;
-			num /= 10;
-
-			s.push(sum);
-		}
-		else
-		{
-			if (num % 1000 >= 10)
-			{
-				sum += num % 10;
-				num /= 10;
-				sum += 2 * num % 10;
-				num /= 100;
-			}
-			else
-			{
-				sum += num % 10;
-				num /= 1000;
-			}
-			s.push(sum);
-		}
+		temp = num.substr(num.size() - (i + 1) * 3, 3);
+		s.push((temp[0] - '0') * 4 + (temp[1] - '0') * 2 + temp[2] - '0');
+	}
+	if (rest == 2)
+	{
+		s.push((num[0] - '0') * 2 + num[1] - '0');
+	}
+	else if (rest == 1)
+	{
+		s.push(num[0] - '0');
 	}
 
 	while (!s.empty())
