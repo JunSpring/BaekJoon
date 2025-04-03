7f58e8414b3ad035b16d8e4fad11380a8cc74485
JunSpring <wjdwnsqja08@gmail.com>
2025-04-03 22:56:24 +0200

    1차원 배열 한 문제 풀기

---
+++ C++/05. 1차원 배열/5597_과제 안 내신 분..?.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<bool> v(30);
+
+    for (int i = 0; i < 28; i++)
+    {
+        int n;
+        cin >> n;
+
+        v[n - 1] = true;
+    }
+
+    for (int i = 0; i < 30; i++)
+        if (!v[i])
+            cout << i + 1 << endl;
+
+    return 0;
+}
