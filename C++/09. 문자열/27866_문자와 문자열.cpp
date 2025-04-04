b49073ecfb25db1fc4062ceaa5974f1857643667
JunSpring <wjdwnsqja08@gmail.com>
2025-04-04 21:18:20 +0200

    문자열 첫 문제 풀기

---
+++ C++/09. 문자열/27866_문자와 문자열.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+
+    int i;
+    cin >> i;
+
+    cout << S[i - 1] << endl;
+
+    return 0;
+}
